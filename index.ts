70afe206d12e8e6a59a2f0d48c06c49b62c8f329
khalil <khalil.haouas@gmail.com>
2025-04-01 13:29:34 +0200

    update

--- index.ts
+++ index.ts
@@ -11,8 +11,8 @@ const app = express();
 
 
 app.use(express.json()); 
-//app.use(cors({    origin: 'http://localhost:3000',  credentials: true  }));
-app.use(cors({ credentials: true, origin: "https://khalil-webdev.de" }));
+app.use(cors({    origin: 'http://localhost:3000',  credentials: true  }));
+ app.use(cors({ credentials: true, origin: "https://khalil-webdev.de" }));
 
 
 app.get('/', (req, res) => {
