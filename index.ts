1b7bcf8a5fb144a44b57cc6b7bcd7df14c406fb7
khalil <khalil.haouas@gmail.com>
2025-04-05 11:54:56 +0200

    update

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json()); 
 //app.use(cors({    origin: 'http://localhost:3000',  credentials: true  }));
 app.use(cors({
-    origin: "https://khalil-webdev.de",
+    origin: ["https://khalil-webdev.de","https://portofolio-db.khalil-webdev.de"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
